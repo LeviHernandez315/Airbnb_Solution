9549c39c7f1e2ed2b7b502679bef8fbd4043da1b
LeviHernandez315 <levi.hernandez@unah.hn>
2025-03-29 01:03:35 -0600

    Agregacion del modelo persona para su impacto en la BD

--- Backend/Infrastructure/Persistence/BackendDBContext.cs
+++ Backend/Infrastructure/Persistence/BackendDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,13 @@ namespace Infrastructure.Persistence
     public class BackendDBContext : DbContext
     {
         public BackendDBContext(DbContextOptions<BackendDBContext> options) : base(options) { }
+
+        public DbSet<Persona> Personas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Queremos que el DNI sea la Primary Key de la tabla Persona
+            modelBuilder.Entity<Persona>().HasKey(p => p.DNI);
+        }
     }
 }
